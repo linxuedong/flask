e656f9f8b335dfa4d0ffd495e177c7e5e45eb2f7
Dawn <xuedonglin1994@gmial.com>
2018-06-19 16:00:53 +0800

    docs: add note of __init__

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,24 +1,41 @@
+# __inti__.py 的两个用途：
+# 1. It will contain the application factory
+# 2. 告诉 Python `flaskr` 文件夹时 Package
+
 import os
 
 from flask import Flask
-
+from . import blog
 
 def create_app(test_config=None):
+    # 创建 flask 实例
     app = Flask(__name__, instance_relative_config=True)
+    # __name__ 为当前 module 的名字
+    # instance_relative_config 告诉 app configuration 文件是相对于 instance 文件夹
+
+
+    # 设置 app 的默认配置
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
     )
+    # SECRET_KEY 用于 保护数据安全
+    # 在 development 时，为了方便设置为 'dev'
+    # 在部署时 应该设置为 random value
 
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
+        # 重写 config.py 文件的配置
     else:
         app.config.from_mapping(test_config)
 
     try:
         os.makedirs(app.instance_path)
+        # 确认 app.instance_path 存在
+        # Flask 不自动创建 instance folder，
+        # 但是 Flask 会在 instance 文件夹中创建 SQLite database 文件
     except OSError:
-        pass
+        print('已经创建 instance 文件夹')
 
     from . import db
     db.init_app(app)
